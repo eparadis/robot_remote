2a78806d72c1a0e234c4ac9d0ef6942434b2f6df
Ed Paradis <legomaniac@gmail.com>
2013-02-09 01:13:32 -0800

    added support for setting PID constants; fixed tricky bug that was causing serial to lock

--- remote.py
+++ remote.py
@@ -6,7 +6,7 @@ if(len(sys.argv) == 3) :
     maxcount = int(sys.argv[2])
 ser.flushInput  # blow away whatever is in the input buffer
 
-speed = 5 # the speed we want to tell the robot to go
+speed = 10 # the speed we want to tell the robot to go
 
 count = 0
 while( count < maxcount) :
@@ -14,16 +14,21 @@ while( count < maxcount) :
     print str(count) + " from bot: " + line
     tokens = line.split(' ')
     #print tokens
+    if( count == 10 ) :
+        # set PID tunings
+        tunings = "K 8.0 0.0 0.0 "   # P I and D
+        print( "-> " + tunings)
+        ser.write( tunings)
     if( tokens[0] == 'LE' ) :
         enc_left = int(tokens[1])
         enc_right = int(tokens[3])
         #print (enc_left, enc_right)
         count = count + 1
-    if( enc_left > 500) :
-        print( "-> L " + str(-speed))
-        ser.write( "L " + str(-speed))
-    if( enc_left < 0 ) :
-        print( "-> L " + str(speed))
-        ser.write( "L " + str(speed))
+        if( enc_left > 500) :
+            print( "-> L " + str(-speed))
+            ser.write( "L " + str(-speed) + " ")
+        if( enc_left < 0 ) :
+            print( "-> L " + str(speed))
+            ser.write( "L " + str(speed) + " ")
 
 
