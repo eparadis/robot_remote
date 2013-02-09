47927fd29e2229250748e1819176c42943b9a3e0
Ed Paradis <legomaniac@gmail.com>
2013-02-08 23:06:32 -0800

    a simple back and forth behavior to test overall loop response

--- remote.py
+++ remote.py
@@ -6,19 +6,24 @@ if(len(sys.argv) == 3) :
     maxcount = int(sys.argv[2])
 ser.flushInput  # blow away whatever is in the input buffer
 
+speed = 5 # the speed we want to tell the robot to go
+
 count = 0
 while( count < maxcount) :
-    line = ser.readline()
+    line = ser.readline().rstrip()
     print str(count) + " from bot: " + line
     tokens = line.split(' ')
     #print tokens
     if( tokens[0] == 'LE' ) :
         enc_left = int(tokens[1])
         enc_right = int(tokens[3])
-        print (enc_left, enc_right)
+        #print (enc_left, enc_right)
         count = count + 1
-    if( count%10 == 9) :
-        # try to reset the encoders
-        print "clearing encoders..."
-        ser.write( "Z " )
+    if( enc_left > 500) :
+        print( "-> L " + str(-speed))
+        ser.write( "L " + str(-speed))
+    if( enc_left < 0 ) :
+        print( "-> L " + str(speed))
+        ser.write( "L " + str(speed))
+
 
