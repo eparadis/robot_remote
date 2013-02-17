a0515740265660bebde1af7e2beadbb70897d68c
Ed Paradis <legomaniac@gmail.com>
2013-02-16 18:31:55 -0800

    add ability to zoom the visualization

--- draw_pos.py
+++ draw_pos.py
@@ -26,6 +26,7 @@ pygame.init()
 fpsClock = pygame.time.Clock()
 windowSurfaceObj = pygame.display.set_mode( (640,480) )
 pygame.display.set_caption( 'Robot Remote Data Visualizer' )
+graphScale = 0.5 
 redColor = pygame.Color(255,0,0)
 whiteColor = pygame.Color(255,255,255)
 blackColor = pygame.Color(0,0,0)
@@ -48,6 +49,7 @@ while True:
     print "distance " + str(Distance( (posAcc[0],posAcc[1]), waypoint))   
     posMsg = "X:{0:.2f}  Y:{1:.2f}  R:{2:.2f}deg".format(*posAcc)
     pidMsg = "Kp:{0:.2f}  Ki:{1:.2f}  Kd:{2:.2f}".format(*kPID)
+    scaleMsg = "vis zoom:{0:.0%} (use -/= to change)".format(graphScale)
 
     # store the position (if its new)
     if deltaPos[0] != 0 or deltaPos[1] != 0 :
@@ -60,7 +62,7 @@ while True:
     windowSurfaceObj.fill( blackColor)
 
     # draw the text message
-    msgSurfaceObj = renderLines( [posMsg, pidMsg], fontObj, False, whiteColor)
+    msgSurfaceObj = renderLines( [posMsg, pidMsg, scaleMsg], fontObj, False, whiteColor)
     #msgSurfaceObj = fontObj.render( msg, False, whiteColor)
     msgRectObj = msgSurfaceObj.get_rect()
     msgRectObj.topleft = (10,10)
@@ -69,7 +71,7 @@ while True:
     # draw all the lines
     A = (0,0)
     for q in lineQueue :
-        B = (q[0],q[1]) 
+        B = (q[0]*graphScale,q[1]*graphScale) 
         pygame.draw.line( windowSurfaceObj, redColor, (A[0]+320,240-A[1]), (B[0]+320,240-B[1]), 2)
         A = B
 
@@ -115,6 +117,11 @@ while True:
             if event.key == K_f :
                 kPID = (kPID[0], kPID[1], kPID[2]-0.01)
                 serialObj.write( CreatePIDTuningsMessage(kPID) )
+
+            if event.key == K_MINUS :
+                graphScale = graphScale / 2.0
+            if event.key == K_EQUALS :
+                graphScale = graphScale * 2.0
 
 
 
