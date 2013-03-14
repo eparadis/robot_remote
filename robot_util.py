6389672ed4832d9f52a79b2d92babe324e3408e3
Ed Paradis <legomaniac@gmail.com>
2013-03-13 22:29:33 -0700

    flesh out some of the functions needed for gathering robot position from an external camera

--- robot_util.py
+++ robot_util.py
@@ -39,3 +39,62 @@ def CreateEncoderZeroMessage() :
     """return a string suitable for sending to the robot"""
     return "Z "
 
+def MakeDataRecord( odoPos, imgFile) :
+    """given (x,y,theta) and a filename, make an entry into the data log"""
+    logfile = "datalog.csv"
+    # format is CSV
+    # Xpos, Ypos, theta, filename
+    # TODO write to the file
+
+def GetPositionsFromLogEntry( line ) :
+    """given a line from the CSV logfile, call the ArUco image processor and return the positions of the static marker, the robot marker, and the robot's position as a list of tuples"""
+    odoPos = (0,0,0)    # X Y and Theta
+    robotMarker = (0,0,0)   # X Y and Theta
+    staticMarker = (0,0,0)  # X Y and Theta
+    # TODO grab the data from the given line
+    line = "1.0,1.0,5,20130308-231905.jpg" # example data
+    lineSplit = string.split(line, ',') 
+    odoPos = (lineSplit[0], lineSplit[1], lineSplit[2])
+    # TODO call ArUco
+    aruco = '/Users/ed/src/opencv/build/aruco-1.2.4/build/utils/aruco_simple'
+    image = '/Users/ed/git/robot_remote/test_frames/' + lineSplit[3]
+    cal = '/Users/ed/git/robot_remote/htc_one_cal/cal.yml'
+    size = '3.622' # side of marker in inches, only counting the black part
+    response = subprocess.check_output([aruco, image, cal, size]).splitlines()
+    for r in response:
+        sp = r.split(',')
+        if(len(sp) >= 14) :
+            markerID = sp[0]
+            T = (sp[9], sp[10], sp[11])
+            R = (sp[12], sp[13], sp[14])
+            print "marker ",markerID," at T",T," R",R 
+            
+            # parse the markers of interest
+            # call a function to transform/project the Txyz,Rxyz data from a marker to the plane X,Y,Theta of the robot positioning code
+            if( markerID == 290)
+                staticMarker = InverseProjection( T, R)
+            if( markerID == 678)
+                robotMarket = InverseProjection( T, R)
+    # return the properly transformed data
+    return (odoPos, robotMarker, staticMarker)
+
+def MeasurePositionOffset( (odoPos, robotMarker, staticMarker) ) :
+    """given the positions of the robotMarker and the staticMarker, calculate where the robot is located..."""
+    # X and Y: static - robot  (ie: distance to robot from static marker)
+    # heading: assume static marker points "north/0deg" so offset the robot's rotation by that much
+    return  (staticMarker[0] - robotMarker[0], staticMarker[1] - robotMarker[1], robotMarker[2] - staticMarker[2] )
+
+def CalculateUMBMarkCorrections( cwOffset, ccwOffset, sideLength ) :
+    """given difference in robot position from start in one dimension (it doesn't matter which) and the length of the side of the square it traveled, return scaling factors for wheelbase and wheel diameters""" 
+    # L = length of the side of the square traveled (4meters in original paper)
+    # x_c.g.cw = average x offset of final robot position during clockwise runs
+    alpha = math.degrees((cwOffset + ccwOffset) / ( -4*sideLength))  # alpha = ( x_c.g.cw + x_c.g.ccw ) / -4*L * (180deg/pi)     (eq 6.26a)
+    beta = math.degrees((cwOffset - ccwOffset) / ( -4*sideLength)) 
+    # wb_act = 90deg/(90deg-alpha)*wb_nom     (eq 6.28)
+    E_b = 90 / (90 - alpha) # E_b = 90deg/(90deg-alpha)             (eq 6.29)
+    R = (sideLength/2) / math.sin(math.radians(beta/2))
+    E_d = (R + beta / 2) / (R - beta / 2)
+    return ( E_b, E_d)
+
+
+
